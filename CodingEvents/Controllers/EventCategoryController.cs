0340ec2ecf519ce94192013f3e1bd64daa473371
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-12 18:04:33 -0400

    added controller methods and templates for creating an event category

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CodingEvents.Controllers
 {
+    [Route("EventCategory")]
     public class EventCategoryController : Controller
     {
         private EventDbContext context;
@@ -24,8 +26,30 @@ namespace CodingEvents.Controllers
         {
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
+        }
 
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new();
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost("create")]
+        public IActionResult ProcessCreateEventCategoryForm(
+            AddEventCategoryViewModel addEventCategoryViewModel
+        )
+        {
+            if (!ModelState.IsValid)
+            {
+                // Send back to form
+                return View("Create", addEventCategoryViewModel);
+            }
+            // Otherwise create eventCategory object, add, and save changes
+            EventCategory eventCategory = new() { Name = addEventCategoryViewModel.Name };
+            context.Categories.Add(eventCategory);
+            context.SaveChanges();
+            return Redirect("/EventCategory");
         }
     }
 }
-
