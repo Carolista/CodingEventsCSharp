db49bee9b99e6ee625f25efc92ae76194cb52772
Carolista <cjones.renaissancewoman@gmail.com>
2024-06-12 18:04:07 -0400

    added ViewModel for event category

---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels;
+
+public class AddEventCategoryViewModel
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must be 3-20 characters long.")]
+    public string? Name { get; set; }
+}
